97f731f2dab84458412aee1ed629e314c5c85646
AgusT613 <torresagustin613@gmail.com>
2023-11-15 11:07:44 -0300

    Vista clientes con sus respectivos datos

---
+++ homebanking/clientes/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [path("<int:user_id>/", views.index, name="index_clientes")]
--- homebanking/clientes/views.py
+++ homebanking/clientes/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Cliente
 
-# Create your views here.
+
+def index(request, user_id):
+    try:
+        cliente = Cliente.objects.get(pk=user_id)
+    except:
+        return render(request, "clientes/cliente.html")
+
+    info_cliente = {
+        "usuario": cliente.user,
+        "tipo_cliente": cliente.tipo_cliente,
+        "nombre": cliente.customer_name,
+        "apellido": cliente.customer_surname,
+        "dni": cliente.customer_dni,
+        "fecha_nacimiento": cliente.dob,
+        "sucursal": cliente.branch_id,
+    }
+
+    context = {"info_cliente": info_cliente}
+
+    return render(request, "clientes/cliente.html", context)
